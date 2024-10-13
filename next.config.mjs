49af4252676d61f82c65fec8caab6f56dcac2c5c
Thomas-Horvath <thomas.horvath0416@gmail.com>
2024-10-13 13:49:15 +0200

    out folder

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath: "/kostold-meg",
-    assetPrefix: "/kostold-meg/",
     output: "export",
     reactStrictMode: true,
 };
