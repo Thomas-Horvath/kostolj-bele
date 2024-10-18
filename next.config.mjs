ed951582352d4b33dbed251e4fd81a93e3cfb3d6
Thomas-Horvath <thomas.horvath0416@gmail.com>
2024-10-18 18:38:48 +0200

    vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
-    basePath: "/kostolj-bele",
-    assetPrefix: "/kostolj-bele/", 
-    images: { unoptimized: true }
 };
 
 export default nextConfig;
